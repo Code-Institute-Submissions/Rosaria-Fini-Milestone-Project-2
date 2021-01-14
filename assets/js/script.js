4bbef17d2af507a1f63a4f3e451bc360ef48c2dd
RosariaFini <rosaria.fini@hotmail.it>
2021-01-14 22:57:35 +0000

    new: added jQuery effects read less/ read more for the features section

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".readmore-btn").on('click', function() {
+      $(this).parent().toggleClass("showContent");
+    
+      
+      /* Shorthand if else statement */
+      var replaceText = $(this).parent().hasClass("showContent") ? "Read Less" : "Read More";
+      $(this).text(replaceText);
+    });
+});
