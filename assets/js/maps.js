fa189d10919fb5500d75b4e2dbb3c310e294d46a
RosariaFini <rosaria.fini@hotmail.it>
2021-01-17 23:07:45 +0000

    new: added new location for map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,7 +10,7 @@ function initMap() {
             var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
             var locations = [
-                { lat: 40.785091, lng: -73.968285 },
+                { lat: 40.96997206917616, lng: 17.174647543893517 },   
                 { lat: 41.084045, lng: -73.874245 },
                 { lat: 40.754932, lng: -73.984016 }
 
@@ -25,7 +25,6 @@ function initMap() {
 
             var markerCluster = new MarkerClusterer(map, markers, 
             {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
-            }
+            } 
 
-
-
+           
