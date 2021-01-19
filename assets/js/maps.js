a348463b88ccaa0e95a45b9f74b9fd3a077941e1
RosariaFini <rosaria.fini@hotmail.it>
2021-01-19 17:51:51 +0000

    new: added new markers for locations on index.html page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,25 +10,29 @@ function initMap() {
             var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
             var locations = [
-                { lat:  40.995399132934665, lng: 17.22094028027515 },   
-                { lat: 38.11637256444217, lng:  13.360550116028266 }, 
-                { lat: 39.22360909816497, lng: 9.120739069039061 },
-                { lat: 36.74573623803619, lng: -3.8799172645394373 },
-                { lat: 41.383328027353535, lng: 2.1877349809692834 }, 
-                { lat: 39.569198067832346, lng: 2.650959684369021 },   
-                { lat: 37.984275443032146, lng: 23.72864048260015 },   
-                { lat: 36.39596237126229, lng: 25.463031208367465 },
-                { lat: 37.77575950347747,  lng: 20.945493259626083 }  
-
-            ];
-
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                position: location,
-                label: labels[i % labels.length], 
-            });
-            }); 
-            
+        { lat: 40.995399132934665, lng: 17.22094028027515, name: "Hello"},
+        { lat: 38.11637256444217, lng: 13.360550116028266, name: "Hello" },
+        { lat: 39.22360909816497, lng: 9.120739069039061, name: "Hello" },
+        { lat: 36.74573623803619, lng: -3.8799172645394373, name: "Hello" },
+        { lat: 41.383328027353535, lng: 2.1877349809692834, name: "Hello" },
+        { lat: 39.569198067832346, lng: 2.650959684369021, name: "Hello" },
+        { lat: 37.984275443032146, lng: 23.72864048260015, name: "Hello" },
+        { lat: 36.39596237126229, lng: 25.463031208367465, name: "Hello" },
+        { lat: 37.77575950347747, lng: 20.945493259626083, name: "Hello" }
+    ];
+    var markers = locations.map(function (location, i) {
+        const infowindow = new google.maps.InfoWindow({
+            content: location.name,
+        });
+        const marker = new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length],
+        });
+        marker.addListener("click", () => {
+            infowindow.open(map, marker);
+        });
+        return marker;
+    });
 
             var markerCluster = new MarkerClusterer(map, markers, 
             {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
