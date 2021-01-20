bf7b3e046c91e075ac5982480ed2d78e2ed94894
RosariaFini <rosaria.fini@hotmail.it>
2021-01-20 20:47:29 +0000

    new: added form for map on index.html, added show more/ show less button for the gallery section on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+     /* Read More / Read Less in the Buched List */
     $(".readmore-btn").on('click', function() {
       $(this).parent().toggleClass("showContent");
     
@@ -7,4 +8,16 @@ $(document).ready(function() {
       var replaceText = $(this).parent().hasClass("showContent") ? "Read Less" : "Read More";
       $(this).text(replaceText);
     });
+
+       /* Show more button / Gallery */
+    $(".gallery-button").on('click', function() {
+        $(this).parent().toggleClass("showMore");
+
+        /* Shorthand if else statement */
+        var replaceButton = $(this).parent().hasClass("showMore") ? "Show Less" : "Show More";
+        $(this).text(replaceButton);
+    });
+
+    
 });
+
